bb2f742843d0f35265916fd2f7d8e5e95f7659bf
MarcoIrineo <marco.irineo228880@potros.itson.edu.mx>
2023-10-30 20:18:10 -0700

    jwt
    se realizo el proceso para el Json Web Token.

---
+++ middlewares/verify-jwt.js
@@ -0,0 +1,30 @@
+const jwt = require('jsonwebtoken');
+const dotenv = require('dotenv');
+dotenv.config();
+
+async function generarToken(user){
+    try{
+        const payload = {
+            nickname: req.nickname,
+            email: req.email
+        }
+        const token = await jwt.sign( payload, process.env.SECRETKEY, {expiresIn: '1h'})
+        return token;
+    } catch(error){
+        throw error;
+    }
+}
+
+async function verifyToken(req, res, next){
+    try{
+        const decoded= await jwt.verify(req.headers.authorization, process.env.SECRETKEY)
+        next()
+    } catch(error){
+        throw error
+    }
+}
+
+module.exports = {
+    generarToken,
+    verifyToken
+}
