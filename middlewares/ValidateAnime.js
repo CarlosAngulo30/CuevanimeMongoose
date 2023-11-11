bf8853f6baf62e8088a76d2cf9d47c5cb15df184
Antonio del Pardo <antonio.delpardo229161@potros.itson.edu.mx>
2023-11-11 10:18:26 -0700

    Change Anime Model
    add images atributte

--- middlewares/ValidateAnime.js
+++ middlewares/ValidateAnime.js
@@ -1,49 +1,58 @@
-const mongoose = require("mongoose")
-const animeDAO = require('../dataAccess/AnimeDAO')
+const mongoose = require("mongoose");
+const animeDAO = require("../dataAccess/AnimeDAO");
 
-function validateCrearAnime(req, res, next){
-    const {nombre, sinopsis, temporada, numerocapitulo} = req.body
-    if(!nombre || !sinopsis || !temporada || !numerocapitulo ){
-        return res.status(400).json({message: "Los campos nombre, sinopsis, temporada, numeroCapitulo son obligatorios"})
-    } 
-    next()
+function validateCrearAnime(req, res, next) {
+  const { nombre, sinopsis, temporada, numerocapitulo, imagenes } = req.body;
+  if (!nombre || !sinopsis || !temporada || !numerocapitulo || !imagenes) {
+    return res.status(400).json({
+      message:
+        "Los campos nombre, sinopsis, temporada, numeroCapitulo e imagenes son obligatorios",
+    });
+  }
+  next();
 }
 
-function validateAnimeId(req, res, next){
-    try {
-        const anime = animeDAO.obtenerAnimePorId(new mongoose.Types.ObjectId(req.params.id));
-        if(!anime){
-            return res.status(404).json({
-                message: 'No existe anime con ese id'
-            })
-        }
-    } catch (error) {
-        return res.status(400).json({message: "El id no es valido"})
+function validateAnimeId(req, res, next) {
+  try {
+    const anime = animeDAO.obtenerAnimePorId(
+      new mongoose.Types.ObjectId(req.params.id)
+    );
+    if (!anime) {
+      return res.status(404).json({
+        message: "No existe anime con ese id",
+      });
     }
-    next()
+  } catch (error) {
+    return res.status(400).json({ message: "El id no es valido" });
+  }
+  next();
 }
 
-function validateActualizarAnime(req, res, next){
-    try {
-        const anime = animeDAO.obtenerAnimePorId(new mongoose.Types.ObjectId(req.params.id));
-        if(!anime){
-            return res.status(404).json({
-                message: 'No existe anime con ese id'
-            })
-        }
-    } catch (error) {
-        return res.status(400).json({message: "El id no es valido"})
+function validateActualizarAnime(req, res, next) {
+  try {
+    const anime = animeDAO.obtenerAnimePorId(
+      new mongoose.Types.ObjectId(req.params.id)
+    );
+    if (!anime) {
+      return res.status(404).json({
+        message: "No existe anime con ese id",
+      });
     }
-    const {nombre, sinopsis, temporada, numeroCapitulo} = req.body
-    if(!nombre || !sinopsis || !temporada || !numeroCapitulo ){
-        return res.status(400).json({message: "Los campos nombre, sinopsis, temporada, numeroCapitulo son obligatorios"})
-    } 
-    next()
+  } catch (error) {
+    return res.status(400).json({ message: "El id no es valido" });
+  }
+  const { nombre, sinopsis, temporada, numeroCapitulo, imagenes } = req.body;
+  if (!nombre || !sinopsis || !temporada || !numeroCapitulo || !imagenes) {
+    return res.status(400).json({
+      message:
+        "Los campos nombre, sinopsis, temporada, numeroCapitulo e imagenes son obligatorios",
+    });
+  }
+  next();
 }
-
 
 module.exports = {
-    validateCrearAnime,
-    validateActualizarAnime,
-    validateAnimeId
-}
+  validateCrearAnime,
+  validateActualizarAnime,
+  validateAnimeId,
+};
--- models/Anime.js
+++ models/Anime.js
@@ -1,22 +1,32 @@
-const mongoose = require('mongoose')
+const mongoose = require("mongoose");
 
-const animeSchema= new mongoose.Schema({
-    nombre:{
-        type: String,
-        required: true
+const animeSchema = new mongoose.Schema({
+  nombre: {
+    type: String,
+    required: true,
+  },
+  sinopsis: {
+    type: String,
+    required: true,
+  },
+  temporada: {
+    type: Number,
+    required: true,
+  },
+  numerocapitulo: {
+    type: Number,
+    required: true,
+  },
+  imagenes: {
+    bannerHorizontal: {
+      type: String,
+      required: true,
     },
-    sinopsis:{
-        type: String,
-        required: true,
+    card: {
+      type: String,
+      required: true,
     },
-    temporada:{
-        type: Number,
-        required:true
-    },
-    numerocapitulo:{
-        type: Number,
-        required:true
-    }
-})
+  },
+});
 
-module.exports=mongoose.model('Anime',animeSchema)
+module.exports = mongoose.model("Anime", animeSchema);
