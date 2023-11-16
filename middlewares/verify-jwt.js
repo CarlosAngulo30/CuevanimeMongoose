f7cf9731e1bdb1237fa76585a0b05ea7293809f7
Antonio del Pardo <antonio.delpardo229161@potros.itson.edu.mx>
2023-11-15 20:46:26 -0700

    Update verify-jwt.js

--- middlewares/verify-jwt.js
+++ middlewares/verify-jwt.js
@@ -1,30 +1,35 @@
-const jwt = require('jsonwebtoken');
-const dotenv = require('dotenv');
+const jwt = require("jsonwebtoken");
+const dotenv = require("dotenv");
 dotenv.config();
 
-async function generarToken(user){
-    try{
-        const payload = {
-            nickname: user.nickname,
-            email: user.email
-        }
-        const token = await jwt.sign( payload, process.env.SECRETKEY, {expiresIn: '1h'})
-        return token;
-    } catch(error){
-        throw error;
-    }
+async function generarToken(user) {
+  try {
+    const payload = {
+      nickname: user.nickname,
+      email: user.email,
+    };
+    const token = await jwt.sign(payload, process.env.SECRETKEY, {
+      expiresIn: "1h",
+    });
+    return token;
+  } catch (error) {
+    throw error;
+  }
 }
 
-async function verifyToken(req, res, next){
-    try{
-        const decoded= await jwt.verify(req.headers.authorization, process.env.SECRETKEY)
-        next()
-    } catch(error){
-        throw error
-    }
+async function verifyToken(req, res, next) {
+  try {
+    const decoded = await jwt.verify(
+      req.headers.authorization,
+      process.env.SECRETKEY
+    );
+    next();
+  } catch (error) {
+    res.status(401).json({ message: "No esta autorizado" });
+  }
 }
 
 module.exports = {
-    generarToken,
-    verifyToken
-}
+  generarToken,
+  verifyToken,
+};
