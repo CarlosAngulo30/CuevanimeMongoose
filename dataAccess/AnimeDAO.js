1b18389206568d4bf51c4a67a98d459f84c6c03c
Antonio del Pardo <antonio.delpardo229161@potros.itson.edu.mx>
2023-11-15 20:45:28 -0700

    Update AnimeDAO.js

--- dataAccess/AnimeDAO.js
+++ dataAccess/AnimeDAO.js
@@ -1,51 +1,61 @@
-const Anime = require('../models/Anime')
-const DataAccessError = require('../errors/DataAccessError')
-const NoDataFoundError = require('../errors/NoDataFoundError')
+const Anime = require("../models/Anime");
+const DataAccessError = require("../errors/DataAccessError");
+const NoDataFoundError = require("../errors/NoDataFoundError");
 
-class AnimeDAO{
-    constructor(){};
+class AnimeDAO {
+  constructor() {}
 
-    static async crearAnime(animeData){
-        try {
-            const anime=new Anime(animeData)
-            return await anime.save()
-        } catch (error) {
-            console.log(error)
-            throw new DataAccessError("Se ha producido un problema al crear el anime")
-        }
+  static async crearAnime(animeData) {
+    try {
+      const anime = new Anime(animeData);
+      return await anime.save();
+    } catch (error) {
+      console.log(error);
+      throw new DataAccessError(
+        "Se ha producido un problema al crear el anime"
+      );
     }
+  }
 
-    static async obtenerAnimePorId(id){
-        try {
-            return await Anime.findById(id)
-        } catch (error) {
-            throw new NoDataFoundError("Se ha producido un problema al obtener el anime")
-        }
+  static async obtenerAnimePorId(id) {
+    try {
+      return await Anime.findById(id);
+    } catch (error) {
+      throw new NoDataFoundError(
+        "Se ha producido un problema al obtener el anime"
+      );
     }
+  }
 
-    static async actualizarAnime(id,animeData){
-        try {
-            return Anime.findByIdAndUpdate(id,animeData,{new:true})
-        } catch (error) {
-            throw new NoDataFoundError("Se ha producido un problema al actualizar el anime")
-        }
+  static async actualizarAnime(id, animeData) {
+    try {
+      return Anime.findByIdAndUpdate(id, animeData, { new: true });
+    } catch (error) {
+      throw new NoDataFoundError(
+        "Se ha producido un problema al actualizar el anime"
+      );
     }
+  }
 
-    static async eliminarAnime(id){
-        try {
-            return Anime.findByIdAndRemove(id)
-        } catch (error) {
-            throw new NoDataFoundError("Se ha producido un problema al eliminar el anime")
-        }
+  static async eliminarAnime(id) {
+    try {
+      return Anime.findByIdAndRemove(id);
+    } catch (error) {
+      throw new NoDataFoundError(
+        "Se ha producido un problema al eliminar el anime"
+      );
     }
+  }
 
-    static async obtenerAnimes(limit=10){
-        try {
-            return await Anime.find().limit(limit)
-        } catch (error) {
-            throw new NoDataFoundError("Se ha producido un problema al obtener todos los animes")
-        }
+  static async obtenerAnimes() {
+    try {
+      return await Anime.find();
+    } catch (error) {
+      throw new NoDataFoundError(
+        "Se ha producido un problema al obtener todos los animes"
+      );
     }
+  }
 }
 
-module.exports = AnimeDAO
+module.exports = AnimeDAO;
