455bd19f6477028172cfc2d848c33ac57fbe7703
Antonio del Pardo <antonio.delpardo229161@potros.itson.edu.mx>
2023-11-15 20:45:18 -0700

    Create MediaContentController.js

---
+++ controllers/MediaContentController.js
@@ -0,0 +1,48 @@
+const MediaContentDAO = require("../dataAccess/MediaContentDAO");
+
+class MediaContentController {
+  static async crearMediaContent(req, res) {
+    try {
+      const mediaContent = await MediaContentDAO.crearMediaContent(req.body);
+      res.status(201).json(mediaContent);
+    } catch (error) {
+      res.status(error.statusCode).json({ message: error.message });
+    }
+  }
+
+  static async obtenerMediaContentById(req, res) {
+    try {
+      const mediaContent = await MediaContentDAO.obtenerMediaContent(
+        req.params.id
+      );
+      res.status(200).json(mediaContent);
+    } catch (error) {
+      res.status(error.statusCode).json({ message: error.message });
+    }
+  }
+
+  static async actualizarMediaContent(req, res) {
+    try {
+      const mediaContent = await MediaContentDAO.actualizarMediaContent(
+        req.params.id,
+        req.body
+      );
+      res.status(200).json(mediaContent);
+    } catch (error) {
+      res.status(error.statusCode).json({ message: error.message });
+    }
+  }
+
+  static async obtenerMediaContentsByIdAnime(req, res) {
+    try {
+      const mediaContents = await MediaContentDAO.obtenerMediaContentsByIdAnime(
+        req.params.idAnime
+      );
+      res.status(200).json(mediaContents);
+    } catch (error) {
+      res.status(error.statusCode).json({ message: error.message });
+    }
+  }
+}
+
+module.exports = MediaContentController;
