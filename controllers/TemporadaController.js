769e87261aa69d81c4ae8bcdd87e0875101164c9
Antonio del Pardo <antonio.delpardo229161@potros.itson.edu.mx>
2023-11-15 20:45:21 -0700

    Create TemporadaController.js

---
+++ controllers/TemporadaController.js
@@ -0,0 +1,33 @@
+const TemporadaDAO = require("../models/TemporadaDAO");
+
+class TemporadaController {
+  static async crearTemporada(req, res) {
+    try {
+      const temporada = await TemporadaDAO.crearTemporada(req.body);
+      res.status(201).json(temporada);
+    } catch (error) {
+      res.status(error.statusCode).json({ message: error.message });
+    }
+  }
+
+  static async obtenerTemporadaById(req, res) {
+    try {
+      const temporada = await TemporadaDAO.obtenerTemporada(req.params.id);
+      res.status(200).json(temporada);
+    } catch (error) {
+      res.status(error.statusCode).json({ message: error.message });
+    }
+  }
+
+  static async obtenerTemporadas(req, res) {
+    try {
+      const limit = req.params.limit || 20;
+      const temporadas = await TemporadaDAO.obtenerTemporadas();
+      res.status(200).json(temporadas);
+    } catch (error) {
+      res.status(error.statusCode).json({ message: error.message });
+    }
+  }
+}
+
+module.exports = TemporadaController;
