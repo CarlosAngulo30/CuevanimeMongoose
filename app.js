8abb733e6ab5410ec254103c8255b607735ff367
Antonio del Pardo <antonio.delpardo229161@potros.itson.edu.mx>
2023-11-15 20:44:36 -0700

    EndPoints
    Creacion de endpoints MediaContent y Temporada

--- app.js
+++ app.js
@@ -1,37 +1,40 @@
-const express = require('express')
-const app = express()
-const morgan = require('morgan')
-const bodyParser = require('body-parser')
-require('dotenv').config({path: './variables.env'})
-const db = require('./config/db')
-const jwt = require('./middlewares/verify-jwt')
+const express = require("express");
+const app = express();
+const morgan = require("morgan");
+const bodyParser = require("body-parser");
+require("dotenv").config({ path: "./variables.env" });
+const db = require("./config/db");
+const jwt = require("./middlewares/verify-jwt");
+const cors = require("cors");
 
 db.conectar();
 
-app.use(bodyParser.json())
-app.use(morgan('combined'))
+app.use(cors());
+app.use(bodyParser.json());
+app.use(morgan("combined"));
 
 //Endpoints
-const AnimeRouter = require('./routes/AnimeRouter')
-app.use('/api/animes', jwt.verifyToken, AnimeRouter)
-const PeliculaRouter = require('./routes/PeliculaRouter')
-app.use('/api/peliculas', jwt.verifyToken, PeliculaRouter)
-const RatingRouter = require('./routes/RatingRouter')
-app.use('/api/ratings', jwt.verifyToken, RatingRouter)
-const ReproductorRouter = require('./routes/ReproductorRouter')
-app.use('/api/reproductors', jwt.verifyToken, ReproductorRouter)
-const SuscripcionRouter = require('./routes/SuscripcionRouter')
-app.use('/api/sucripcions', jwt.verifyToken, SuscripcionRouter)
-const UsuarioRouter = require('./routes/UsuarioRouter')
-app.use('/api/usuarios', UsuarioRouter)
+const AnimeRouter = require("./routes/AnimeRouter");
+app.use("/api/animes", AnimeRouter);
+const MediaContentRouter = require("./routes/MediaContentRouter");
+app.use("/api/mediaContents", MediaContentRouter);
+const PeliculaRouter = require("./routes/PeliculaRouter");
+app.use("/api/peliculas", jwt.verifyToken, PeliculaRouter);
+const RatingRouter = require("./routes/RatingRouter");
+app.use("/api/ratings", jwt.verifyToken, RatingRouter);
+const ReproductorRouter = require("./routes/ReproductorRouter");
+app.use("/api/reproductors", jwt.verifyToken, ReproductorRouter);
+const SuscripcionRouter = require("./routes/SuscripcionRouter");
+app.use("/api/sucripcions", jwt.verifyToken, SuscripcionRouter);
+const UsuarioRouter = require("./routes/UsuarioRouter");
+app.use("/api/usuarios", UsuarioRouter);
 
+app.use((req, res) => {
+  res.status(400).send("Ruta no encontrada");
+});
 
-app.use((req, res)=>{
-    res.status(400).send("Ruta no encontrada")
-})
+const port = process.env.PORT || 3000;
 
-const port = process.env.PORT || 3000
-
-app.listen(port, ()=>{
-    console.log('Servidor corriendo en el puerto '+ port)
-})
+app.listen(port, () => {
+  console.log("Servidor corriendo en el puerto " + port);
+});
